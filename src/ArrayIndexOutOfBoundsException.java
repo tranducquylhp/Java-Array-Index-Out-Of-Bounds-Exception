fc5da4b4793e0327ad0176254dc5bff8c09cd6c2
unknown <tranducquylhp@gmail.com>
2019-07-24 08:28:03 +0700

    ok

---
+++ src/ArrayIndexOutOfBoundsException.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class ArrayIndexOutOfBoundsException {
+
+    public static int[] createRandom() {
+        Random rd = new Random();
+        int[] arr = new int[100];
+        System.out.println("Danh sách phần tử của mảng: " );
+        for (int i = 0; i < 100; i++) {
+            arr[i] = rd.nextInt(100);
+            System.out.print(arr[i]+ " ");
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        ArrayIndexOutOfBoundsException arrExample = new ArrayIndexOutOfBoundsException();
+        int[] arr = ArrayIndexOutOfBoundsException.createRandom();
+
+        Scanner scaner = new Scanner(System.in);
+        System.out.println("\nVui lòng nhập chỉ số của một phần tử bất kỳ: ");
+        int x = scaner.nextInt();
+        try {
+            System.out.println("Giá trị của phần tử có chỉ số " + x + " là " + arr[x]);
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("Chỉ số vượt quá giới hạn của mảng");
+        }
+    }
+}
